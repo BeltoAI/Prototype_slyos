4b7a49389b041e34e149dc9c5f99dfcf872c3f29
BeltoAI <you@example.com>
2025-09-22 19:05:39 -0700

    Fix: ESM next.config (mjs) for type:module

--- web/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Make it crystal-clear these APIs are runtime:
-  experimental: { serverActions: { allowedOrigins: ['*'] } },
-};
-module.exports = nextConfig;
